eb6b292808e4b33e2846b26f7d2ee26b799eae84
electrouncle <boris.janek@googlemail.com>
2025-03-28 15:08:36 +0100

    Verbessere Server-Konfiguration

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware für JSON-Parsing
+app.use(express.json());
+
+// Basis-Route
 app.get('/', (req, res) => {
     res.send('Willkommen bei meiner ersten Web-Anwendung!');
 });
@@ -14,6 +18,18 @@ app.get('/addiere/:a/:b', (req, res) => {
     res.json({ ergebnis });
 });
 
-app.listen(port, () => {
+// Fehlerbehandlung
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Etwas ist schief gelaufen!');
+});
+
+// 404 Handler
+app.use((req, res) => {
+    res.status(404).send('Seite nicht gefunden');
+});
+
+// Server starten
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server läuft auf Port ${port}`);
 });
